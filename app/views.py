ea9d771567bd1259879d7dacbd4bec39d7e15ae2
hrs <hruthik.s@oracle.com>
2024-05-28 00:46:37 +0530

    contact us and nav bar effects, migrating the files into proper folders

--- app/views.py
+++ app/views.py
@@ -17,59 +17,74 @@ def home():
 
 @bp.route('/flights')
 def flights():
-    return render_template('flights.html')
+    return render_template('services/flights.html')
 
 
 @bp.route('/hotels')
 def hotels():
-    return render_template('hotels.html')
+    return render_template('services/hotels.html')
 
 
 @bp.route('/holidays')
 def holidays():
-    return render_template('holidays.html')
+    return render_template('services/holidays.html')
 
 
 @bp.route('/insurance')
 def insurance():
-    return render_template('insurance.html')
+    return render_template('services/insurance.html')
 
 
 @bp.route('/visa')
 def visa():
-    return render_template('visa.html')
+    return render_template('services/visa.html')
+
+
+@bp.route('/domestic')
+def domestic():
+    return render_template('services/domestic.html')
+
+
+@bp.route('/international')
+def international():
+    return render_template('services/international.html')
 
 
 @bp.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('company/contact.html')
 
 
 @bp.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('company/about.html')
 
 
 @bp.route('/spain')
 def spain():
-    return render_template('spain.html')
+    return render_template('packages/spain.html')
 
 
 @bp.route('/dubai')
 def dubai():
-    return render_template('dubai.html')
+    return render_template('packages/dubai.html')
 
 
 @bp.route('/ourstory')
 def ourstory():
-    return render_template('ourstory.html')
+    return render_template('company/ourstory.html')
 
 
 @bp.route('/ourservices')
 def ourservices():
-    return render_template('ourservices.html')
+    return render_template('company/ourservices.html')
 
 
 @bp.route('/faq')
 def faq():
-    return render_template('faq.html')
+    return render_template('company/faq.html')
+
+
+@bp.route('/dubai4n5d')
+def dubai4n5d():
+    return render_template('packages/dubai4n5d.html')
